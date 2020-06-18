d4b9d7e4a800e64175e4d9a5ae37fd30e7ea29f8
vik-mol <viktorvmoll@gmail.com>
2020-06-18 16:04:50 +0700

    Updates

---
+++ precache-manifest.ca2b0f3c74902543260ab9f36576b7f4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a4a758e669441c87e3ec5b7e8fcbd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a2da65ec2eed95f346f",
+    "url": "/static/css/2.89fcc9a3.chunk.css"
+  },
+  {
+    "revision": "545194fc674936ddc8da",
+    "url": "/static/css/main.576fad07.chunk.css"
+  },
+  {
+    "revision": "2a2da65ec2eed95f346f",
+    "url": "/static/js/2.5c823e26.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5c823e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545194fc674936ddc8da",
+    "url": "/static/js/main.9b05cc1c.chunk.js"
+  },
+  {
+    "revision": "3788c068654579cd309f",
+    "url": "/static/js/runtime-main.9fd072ae.js"
+  },
+  {
+    "revision": "45656b73e1a07f979bfb56559b359b07",
+    "url": "/static/media/Geometria-Medium.45656b73.woff"
+  },
+  {
+    "revision": "86b1b85bdce1520f605f5ebe8ba1000c",
+    "url": "/static/media/Geometria-Medium.86b1b85b.ttf"
+  },
+  {
+    "revision": "90152a4f5c76144f11d47dcf43ae12d9",
+    "url": "/static/media/Geometria-Medium.90152a4f.eot"
+  },
+  {
+    "revision": "3c6a7d3ae76a5aec78aac69db1af15cf",
+    "url": "/static/media/Geometria.3c6a7d3a.woff"
+  },
+  {
+    "revision": "4307d7bad82729169e304b76363c6df6",
+    "url": "/static/media/Geometria.4307d7ba.ttf"
+  },
+  {
+    "revision": "778525f6405ea0daa98d885a4ba17189",
+    "url": "/static/media/Geometria.778525f6.eot"
+  },
+  {
+    "revision": "127c337c0bdccbe16d0ec3892728c6a0",
+    "url": "/static/media/logo.127c337c.svg"
+  },
+  {
+    "revision": "f81ce245a526d74af4abf3297fb47c04",
+    "url": "/static/media/logoM.f81ce245.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
